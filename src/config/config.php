8fe6d8ba3674b8d207dafcf9c137611e80f9180e
Wojciech <wojciech.dziwoki@gmail.com>
2021-07-07 15:14:02 +0200

    dodanie dump.sql

--- src/config/config.php
+++ src/config/config.php
@@ -1,9 +1,9 @@
 <?php
 
-define('DB_SERVER', '185.208.164.127');
-define('DB_USERNAME', 'sancrow_root');
-define('DB_PASSWORD', '1Ketiow1');
-define('DB_NAME', 'sancrow_prodyct-catalog');
+define('DB_SERVER', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', 'root');
+define('DB_NAME', 'product-catalog');
 
 /* Attempt to connect to MySQL database */
 $mysqli = new mysqli(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
